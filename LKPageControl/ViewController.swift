efa508f50721b47608b1483907ddec08c1c3d865
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-macbook-pro.local>
2018-09-13 22:01:51 +0530

    Initial Commit

---
+++ LKPageControl/ViewController.swift
@@ -0,0 +1,28 @@
+//ViewController.swift
+/*
+ * LKPageControl
+ * Created by penumutchu.prasad@gmail.com on 13/09/18
+ * Is a product created by abnboys
+ * For the  in the LKPageControl
+ 
+ * Here the permission is granted to this file with free of use anywhere in the IOS Projects.
+ * Copyright © 2018 ABNBoys.com All rights reserved.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
